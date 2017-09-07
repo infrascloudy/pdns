5fba03db57cdfaa4236675561b1078e3c352566b
allan swanepoel <Allanice001@gmail.com>
2017-09-07 18:06:45 +0200

    first commit

---
+++ docs/source/index.rst
@@ -0,0 +1,17 @@
+Python DNS
+==========
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+.. only:: html
+
+   Indices and tables
+   ==================
+
+   * :ref:`genindex`
+   * :ref:`modindex`
+   * :ref:`search`
---
+++ pdns/metadata.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'pdns'
+project = "Python DNS"
+project_no_spaces = project.replace(' ', '')
+version = '0.1.0'
+description = 'It does dns things'
+authors = ['Allan Swanepoel']
+authors_string = ', '.join(authors)
+emails = ['support@linaccess.za.net']
+license = 'MIT'
+copyright = '2017 ' + authors_string
+url = 'http://infrascloudy.info/'
